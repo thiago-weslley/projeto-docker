9b797f384268875378ab32beadcddbebf33186bf
root <root@ThW.localdomain>
2023-03-02 16:47:24 -0300

    adicionando o script de criação e configuração inicial do container incluindo a página web

---
+++ comandos/script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo '=== adicionado a imagem http ao repositório local ==='
+docker pull httpd
+
+echo '=== verificando as imagens no repositório local ==='
+docker images
+
+echo '=== colocando o container em execução ==='
+docker run --name webserver -d -p 8080:80 httpd
+
+echo '=== validando os containers em execução ==='
+docker ps
+
+echo '=== instalando o unzip e wget ==='
+apt install unzip
+apt install wget
+
+echo '=== criando uma pasta para receber o arquivo zip com a página html ==='
+mkdir /website
+cd /website
+wget https://github.com/digitalinnovationone/cv/archive/refs/heads/main.zip
+
+echo '=== extraindo o arquivo zip ==='
+unzip main.zip
+
+echo '=== finalizando esta estapa ==='
